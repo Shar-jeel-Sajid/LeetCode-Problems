eec314933f9c9775e639547c118038ed456ae754
Shar-jeel-Sajid <135236134+Shar-jeel-Sajid@users.noreply.github.com>
2024-03-15 19:16:55 +0500

    Time: 26 ms (6.72%) | Memory: 28.8 MB (5.60%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        long product = 1;
-        int countZero = 0;
+        if(nums.size() <= 1)
+            return nums;
         vector<int> answers;
-        for(int i = 0;i < nums.size();i++){
-            if(nums[i])
-                product *= nums[i];
-            else {
-                countZero++;
-            }
-        }
-        for(int i = 0;i < nums.size();i++) {
-            if(nums[i]) {
-                if(!countZero)
-                    answers.push_back(product/nums[i]);
-                else
-                    answers.push_back(0);
-            }
-            else {
-                if(countZero == 1)
-                    answers.push_back(product);
-                else
-                    answers.push_back(0);
-            }
-        }
+        answers.push_back(1);
+        answers.push_back(nums[0]);
+        for(int i = 2;i < nums.size();i++)
+            answers.push_back(answers[i-1]*nums[i-1]);
+        vector<int> right;
+        for(int i=0;i<nums.size();i++)
+            right.push_back(1);
+        right[right.size()-2]=(nums[nums.size()-1]);
+        for(int i = nums.size()-2;i >= 0;i--)
+            right[i] = nums[i+1]*right[i+1];
+        for(int i = 0;i < answers.size();i++)
+            answers[i] *= right[i];
         return answers;
+        
     }
 };
