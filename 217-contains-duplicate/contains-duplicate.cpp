9bd5dac14d92150428c3f41719db2e1b6e339a63
Shar-jeel-Sajid <135236134+Shar-jeel-Sajid@users.noreply.github.com>
2024-03-17 15:35:22 +0500

    Time: 91 ms (64.26%) | Memory: 73.1 MB (60.35%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> freq;
+        for(int &i:nums)
+            if(freq.find(i) != freq.end())
+                return true;
+            else
+                freq.insert(i);
+        return false;
+    }
+};
