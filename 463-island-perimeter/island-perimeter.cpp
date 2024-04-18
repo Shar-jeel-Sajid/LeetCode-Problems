a24379137f4c0311815b213230ee1d1d91aa4c80
Shar-jeel-Sajid <135236134+Shar-jeel-Sajid@users.noreply.github.com>
2024-04-18 20:05:21 +0500

    Time: 73 ms (60.13%) | Memory: 102.3 MB (25.30%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -21,11 +21,6 @@ public:
                 }
             }
         }
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) 
-                cout << grid[i][j];
-            cout << endl;
-        }
         return sum;
     }
 };
