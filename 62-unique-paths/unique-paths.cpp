b5025a3da8cf07f0f715d921ac849d0c06151791
Shar-jeel-Sajid <135236134+Shar-jeel-Sajid@users.noreply.github.com>
2024-05-01 21:17:56 +0500

    Time: 4 ms (13.13%) | Memory: 7.7 MB (45.94%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, vector<int>(n, 0));
+        grid[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (m - i - 1)
+                    grid[i + 1][j] += grid[i][j];
+                if (n - 1 - j)
+                    grid[i][j + 1] += grid[i][j];
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
+        return grid[m - 1][n - 1];
+    }
+};
