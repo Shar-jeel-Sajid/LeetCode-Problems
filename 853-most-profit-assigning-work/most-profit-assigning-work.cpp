6d05fa8134c0597dba6a058fd85e91e4d4bb3457
Shar-jeel-Sajid <135236134+Shar-jeel-Sajid@users.noreply.github.com>
2024-06-18 15:29:02 +0500

    Time: 287 ms (18.81%) | Memory: 41.2 MB (47.29%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,33 @@
+// Comparator for priority queue based on key (min-heap by default)
+struct Compare {
+    bool operator()(const pair<int,int>& lhs, const pair<int,int>& rhs) const {
+        return lhs.first > rhs.first; 
+    }
+};
+
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int sum = 0;
+        sort(worker.begin(),worker.end());
+        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> q;
+        int n = profit.size(),m = worker.size();
+        for(int i = 0;i < n;i++) {
+            q.push(pair<int,int>(difficulty[i],profit[i]));
+        }
+        for(int i = 0;i < m;i++){
+            int maxi = 0;
+            while(true) {
+                cout << worker[i] << " " << difficulty[i] << " " << profit[i] << endl;
+                if(q.empty() || q.top().first > worker[i]) {
+                    sum += maxi;
+                    q.push(pair<int,int>(worker[i],maxi));
+                    break;
+                }
+                maxi = max(q.top().second,maxi);
+                q.pop();
+            }
+        }
+        return sum;
+    }
+};
